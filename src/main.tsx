00b5fb9275c5669720de3c2fcb5f6f2e1330a596
Ilya Baybekov <baybekov.ilya@mail.ru>
2025-06-18 22:59:46 +0300

    0.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import { queryClient } from "./utils/queryClient";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </StrictMode>
 );
